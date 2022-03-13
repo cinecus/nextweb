9f6bbd45237ffc886c96201309f3301ba974092a
Chananon Chantaratin <chananon.seen@gmail.com>
2022-03-13 15:10:46 +0700

    next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
 const withImages = require("next-images");
 const withTM = require("next-transpile-modules")(["@madzadev/image-slider"]);
